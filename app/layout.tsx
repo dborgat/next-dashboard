82213b92373e142fd3da917368d18e0ca6b03639
dborgat <dborgat@gmail.com>
2024-07-01 10:30:26 -0300

    refactor: Update layout styles and add theme configuration

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,9 @@ import { Comfortaa } from 'next/font/google';
 import './ui/globals.css';
 import Navbar from './ui/dashboard/navbar/navbar';
 import { UserProvider } from './context/UserContext';
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from '../theme';
 
 const conforta = Comfortaa({
   subsets: ['latin'],
@@ -21,9 +24,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className={`${conforta.className} font-bold bg-slate-100`}>
-        <Navbar />
-        <UserProvider>{children}</UserProvider>
+      <body className={`${conforta.className} font-bold bg-slate-300 h-screen`}>
+        <AppRouterCacheProvider>
+          <ThemeProvider theme={theme}>
+            <UserProvider>{children}</UserProvider>
+          </ThemeProvider>
+        </AppRouterCacheProvider>
       </body>
     </html>
   );
---
+++ theme.ts
@@ -0,0 +1,16 @@
+'use client';
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: "#FF4A1C"
+        },
+        secondary: {
+            main: "#2667FF"
+        }
+    },
+
+});
+
+export default theme;
