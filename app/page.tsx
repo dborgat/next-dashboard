dd516e93dae15591ea89f27fc319040953d61221
dborgat <dborgat@gmail.com>
2024-07-01 10:30:33 -0300

    refactor: Update login and register page styles and functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,43 @@
-import Image from "next/image";
+'use client';
+import * as React from 'react';
+import {
+  Container,
+  Box,
+  Stack,
+  TextField,
+  Button,
+  Typography,
+} from '@mui/material';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     <div className="bg-red-400">
-      aca estoy
-     </div>
-    </main>
+    <Container
+      sx={{
+        alignContent: 'center',
+        height: '100vh',
+      }}
+    >
+      <Typography variant='h4' gutterBottom align='center'>
+        Inicia sesión o registrate para comenzar a ayudar a los animales 
+      </Typography>
+      <Box sx={{ borderRadius: 1, backgroundColor: 'white', padding: 4 }}>
+        <Stack spacing={2}>
+          <TextField
+            required
+            id='outlined-required'
+            label='Email'
+            defaultValue=''
+          />
+          <TextField
+            required
+            id='outlined-password-input'
+            label='Password'
+            type='password'
+            autoComplete='current-password'
+          />
+          <Button variant='contained'>Ingresar</Button>
+        </Stack>
+      </Box>
+    </Container>
   );
 }
--- app/ui/dashboard/rescues/newRescuePage.tsx
+++ app/ui/dashboard/rescues/newRescuePage.tsx
@@ -1,5 +1,6 @@
 'use client';
-import React from 'react';
+import React, { useState } from 'react';
+import { useRouter } from 'next/navigation';
 
 interface NewRescuesPageProps {
   zones: [{ id: number; name: string }];
@@ -10,13 +11,53 @@ const NewRescuesPage: React.FC<NewRescuesPageProps> = ({
   zones,
   animalType,
 }) => {
+  const router = useRouter();
+  const [form, setForm] = useState({
+    zone: '',
+    totalAmount: '',
+  });
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  console.log('asdasdasdasdasdasd', form);
+
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+    setLoading(true);
+    // try {
+    //   const response = await fetch('/api/addRescue', {
+    //     method: 'POST',
+    //     headers: {
+    //       'Content-Type': 'application/json',
+    //     },
+    //     body: JSON.stringify(form),
+    //   });
+
+    //   if (response.ok) {
+    //     setLoading(false);
+    //     router.push('/dashboard/addRescataditos');
+    //   }
+    // } catch (e) {
+    //   setLoading(false);
+    //   console.log(e);
+    // }
+  };
+
   return (
     <div className='p-5'>
       <div className='flex justify-between mb-5'>
         <h1 className='text-3xl mb-5'>Agregar un nuevo Rescate</h1>
       </div>
       <div className='bg-slate-950 flex flex-col gap-5 p-5 shadow-xl w-full'>
-        <form className='grid grid-cols-2 gap-5'>
+        <form className='grid grid-cols-2 gap-5' onSubmit={handleSubmit}>
           <div className='grid gap-1'>
             <label className='text-sm text-gray-200'>
               Selecciona la zona de rescate
@@ -24,8 +65,8 @@ const NewRescuesPage: React.FC<NewRescuesPageProps> = ({
             <select
               className='bg-gray-50 border-r-8 border-transparent text-gray-900 text-sm block w-full p-2.5'
               name='zone'
-              //   onChange={handleChange}
-              //   value={form.zone}
+              onChange={handleChange}
+              value={form.zone}
               required
             >
               <option value='' disabled hidden>
@@ -46,57 +87,16 @@ const NewRescuesPage: React.FC<NewRescuesPageProps> = ({
               type='text'
               className='bg-gray-50 border-r-8 border-transparent text-gray-900 text-sm block w-full p-2.5'
               placeholder='Monto total del rescate $'
+              name='totalAmount'
+              onChange={handleChange}
+              value={form.totalAmount}
             />
-          </div>
-          <div className='grid gap-1'>
-            <label className='text-sm text-gray-200'>
-              Cantidad de rescataditos
-            </label>
-            <select
-              className='bg-gray-50 border-r-8 border-transparent text-gray-900 text-sm block w-full p-2.5'
-              name='quantity'
-              //   onChange={handleChange}
-              //   value={form.zone}
-              required
-            >
-              <option value='' disabled hidden>
-                Cantidad de rescataditos
-              </option>
-              {Array(6)
-                .fill('')
-                .map((_, index) => (
-                  <option key={index} value={index}>
-                    {index}
-                  </option>
-                ))}
-            </select>
-          </div>
-          <div className='grid gap-1'>
-            <label className='text-sm text-gray-200'>
-              Selecciona un tipo de animal
-            </label>
-            <select
-              className='bg-gray-50 border-r-8 border-transparent text-gray-900 text-sm block w-full p-2.5'
-              name='animalType'
-              //   onChange={handleChange}
-              //   value={form.zone}
-              required
-            >
-              <option value='' disabled hidden>
-                Selecciona un tipo de animal
-              </option>
-              {animalType.map(({ id, type }) => (
-                <option key={id} value={id}>
-                  {type}
-                </option>
-              ))}
-            </select>
           </div>
           <button
             className={`p-2 bg-red-300 mt-5 col-span-2 flex gap-5 justify-center items-center`}
             type='submit'
           >
-            Crear rescate
+            Crear rescate y sumar los rescataditos
           </button>
         </form>
       </div>
